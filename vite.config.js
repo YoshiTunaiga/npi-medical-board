a25faaa123f2fc87115d50197063a34084df6254
Gi Diaz <85481752+YoshiTunaiga@users.noreply.github.com>
2024-07-29 19:11:04 -0400

    fix: base url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/npi-medical-board",
+  // base: "/npi-medical-board",
   plugins: [react()],
   server: {
     proxy: {
