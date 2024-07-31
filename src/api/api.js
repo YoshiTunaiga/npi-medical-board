14f6383f6386a88697659dace53e597a3ed7343d
Gi Diaz <85481752+YoshiTunaiga@users.noreply.github.com>
2024-07-30 22:06:55 -0400

    fix: vite api structure

--- src/api/api.js
+++ src/api/api.js
@@ -1,21 +1,16 @@
 import axios from "axios";
 
-// const API_BASE_URL = "https://npi-db.org"; // backend API URL
-
 export default async function fetchDoctorInfo(npId) {
+  let config = {
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `/api/${npId}`,
+    headers: {},
+  };
   try {
-    const config = {
-      method: "get",
-      url: `/api/${npId}`,
-      headers: {
-        "Cache-Control": "no-cache",
-        "Content-Type": "application/json",
-      },
-    };
-    const response = await axios(config);
+    const response = await axios.request(config);
     return response.data;
   } catch (error) {
-    console.error("Error fetching doctor info:", error);
-    throw error;
+    console.log(error);
   }
 }
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/npi-medical-board/",
+  base: "/npi-medical-board",
   plugins: [react()],
   server: {
-    host: true,
     proxy: {
       "/api": {
         target: "https://npi-db.org",
